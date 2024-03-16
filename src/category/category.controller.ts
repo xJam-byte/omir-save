a6ebe93cdaf27875deeb335d50cf24af93fe25cc
xJam-byte <abylaimuratbayev@gmail.com>
2024-03-16 14:51:42 +0600

    category controller and service

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from "@nestjs/common";
+import { CategoryService } from "./category.service";
+import { CreateCategoryDto } from "./Dto/create-category-dto";
 
-@Controller('category')
-export class CategoryController {}
+@Controller("category")
+export class CategoryController {
+  constructor(private categoryService: CategoryService) {}
+
+  @Post()
+  create(@Body() dto: CreateCategoryDto) {
+    return this.categoryService.createMedic(dto);
+  }
+
+  @Get()
+  getAll() {
+    return this.categoryService.getAllMedics();
+  }
+}
--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,4 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/sequelize";
+import { Category } from "./category.model";
+import { CreateCategoryDto } from "./Dto/create-category-dto";
 
 @Injectable()
-export class CategoryService {}
+export class CategoryService {
+  constructor(
+    @InjectModel(Category) private categoryRepository: typeof Category
+  ) {}
+
+  async createMedic(dto: CreateCategoryDto) {
+    const user = await this.categoryRepository.create(dto);
+    return user;
+  }
+  async getAllMedics() {
+    const users = await this.categoryRepository.findAll();
+    return users;
+  }
+}
