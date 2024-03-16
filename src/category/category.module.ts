29f91dc55ae0c325088764754a51125d1b1adf27
xJam-byte <abylaimuratbayev@gmail.com>
2024-03-16 14:52:52 +0600

    true category

--- src/category/category.module.ts
+++ src/category/category.module.ts
@@ -1,9 +1,12 @@
-import { Module } from '@nestjs/common';
-import { CategoryController } from './category.controller';
-import { CategoryService } from './category.service';
+import { Module } from "@nestjs/common";
+import { CategoryController } from "./category.controller";
+import { CategoryService } from "./category.service";
+import { Category } from "./category.model";
+import { SequelizeModule } from "@nestjs/sequelize";
 
 @Module({
   controllers: [CategoryController],
-  providers: [CategoryService]
+  providers: [CategoryService],
+  imports: [SequelizeModule.forFeature([Category])],
 })
 export class CategoryModule {}
